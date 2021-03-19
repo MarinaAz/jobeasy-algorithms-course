8ca8d265ee3f13b754ff23f42ac59acd1dcd2cc7
MarinaAz <mariaziz69@gmail.com>
2021-03-19 17:19:32 -0400

    lesson_5_HW_3

---
+++ lesson_5/HW_3/recursion_fib.py
@@ -0,0 +1,13 @@
+# Recursion for Fibonacci,
+
+def fibonacci_rec(num):
+    if num < 0:
+        return 'Wrong value'
+    if num == 0:
+        return 0
+    if num == 1 or num == 2:
+        return 1
+    return fibonacci_rec(num - 1) + fibonacci_rec(num - 2)
+
+
+print(fibonacci_rec(20))
