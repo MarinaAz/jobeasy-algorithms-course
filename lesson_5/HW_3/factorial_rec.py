1a5374f168cda0df4b83d69bfd3019c31983c615
MarinaAz <mariaziz69@gmail.com>
2021-03-19 17:17:53 -0400

    lesson_5_HW_3

---
+++ lesson_5/HW_3/factorial_rec.py
@@ -0,0 +1,11 @@
+# Recursion for factorial
+
+def factorial_rec(num):
+    if num < 0:
+        return 'Only positive number'
+    if num == 0 or num == 1:
+        return 1
+    return num * factorial_rec(num - 1)
+
+
+print(factorial_rec(5))
